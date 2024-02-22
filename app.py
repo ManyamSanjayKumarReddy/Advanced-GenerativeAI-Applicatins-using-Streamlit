4c46e858d519a05816aeb605a652be1e1dea2c2f
ManyamSanjayKumarReddy <entrepreneur.sanju@gmail.com>
2024-02-22 22:05:19 +0530

    Streamlit version change

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 if st.button("Home"):
-    st.switch_page("main.py")
+    st.switch_page("app.py")
 if st.button("Youtube Video Summarizer"):
     st.switch_page("pages/yt_summarizer.py")
 if st.button("PDF File Content Summarizer"):
